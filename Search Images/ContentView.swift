ca93f2657ea1ecf41a0af598304c0404570aa57b
Dmitry Balantaev <dmitrybalantaev@gmail.com>
2022-01-27 20:50:00 +0600

    Initial Commit

---
+++ Search Images/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Search Images
+//
+//  Created by Дмитрий Балантаев on 27.01.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Search Images/Search_ImagesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Search_ImagesApp.swift
+//  Search Images
+//
+//  Created by Дмитрий Балантаев on 27.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct Search_ImagesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
